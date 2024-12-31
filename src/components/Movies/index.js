1d75116ca9ed7c0c78754776b4ac7edd88d9178c
yy <139915418+yydchang@users.noreply.github.com>
2024-12-31 03:06:19 +0000

    Created dynamically generated nav bar for Movies component

--- src/components/Movies/index.js
+++ src/components/Movies/index.js
@@ -1,7 +1,25 @@
+import { NavLink, Route, Switch } from "react-router-dom";
+import MovieDetails from "../MovieDetails";
+import { movies } from "../../data/movieData";
+
 function Movies({ movies: { id, title, description } }) {
   return (
     <div className="comp orange">
       <h1>Movies Component</h1>
+      <nav>
+        <ul>
+          {movies.map((movie) => (
+            <li>
+              <NavLink to={`/movies/${movie.id}`}>{movie.title}</NavLink>
+            </li>
+          ))}
+        </ul>
+      </nav>
+      <Switch>
+        <Route path="/movies/:movieId">
+          <MovieDetails />
+        </Route>
+      </Switch>
     </div>
   );
 }
